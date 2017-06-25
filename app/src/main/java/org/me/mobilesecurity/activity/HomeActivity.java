64984c34f46d0eafbab557513002bfdb5e77754c
Zhanlong Yang <673029686@qq.com>
2017-06-25 23:00:36 +0800

    Add GridViewData to HomeActivity

--- app/src/main/java/org/me/mobilesecurity/activity/HomeActivity.java
+++ app/src/main/java/org/me/mobilesecurity/activity/HomeActivity.java
@@ -4,13 +4,42 @@ import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.me.mobilesecurity.R;
+import org.me.mobilesecurity.bean.HomeBean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     private ImageView ivLogon;
+    private GridView mGridView;
+    private List<HomeBean> mDatas;
+
+    private int[] ICONS = new int[]{
+            R.drawable.sjfd, R.drawable.srlj,
+            R.drawable.rjgj, R.drawable.jcgl,
+            R.drawable.lltj, R.drawable.sjsd,
+            R.drawable.hcql, R.drawable.cygj};
+
+    private String[] TITLES = new String[] {
+            "手机防盗", "骚扰拦截",
+            "软件管家", "进程管理",
+            "流量统计", "手机杀毒",
+            "缓存清理", "常用工具"};
+
+    private String[] DESCS = new String[] {
+            "远程定位手机", "全面拦截骚扰",
+            "管理您的软件", "管理运行进程",
+            "流量一目了然", "病毒无处藏身",
+            "系统快如火箭", "工具大全"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +50,35 @@ public class HomeActivity extends AppCompatActivity {
 
         // 让logo进行动画
         doLogoAnimation();
+
+        // 给gridView加载数据
+        initGridViewData();
     }
 
-    private void initView(){
+    private void initView() {
         ivLogon = (ImageView) findViewById(R.id.home_iv_logo);
+        mGridView = (GridView) findViewById(R.id.home_gridview);
+    }
+
+    private void initGridViewData() {
+        // 给list数据初始化
+        mDatas = new ArrayList<>();
+        for (int i = 0; i < ICONS.length; i++) {
+            HomeBean bean = new HomeBean();
+            bean.setIcon(ICONS[i]);
+            bean.setTitle(TITLES[i]);
+            bean.setDesc(DESCS[i]);
+            mDatas.add(bean);
+        }
+
+        // gridview 和listView数据加载方式相似
+        // 给gridview设置Adapter
+        // Adapter的设计模式
+        // 1. 实现adapter
+        // 2. adapter 需要依赖List<T>数据源
+        // 3. 确定List中item的数据类型
+        // 4. item条目的UI显示---> bean
+        mGridView.setAdapter(new HomeAdapter());
     }
 
     private void doLogoAnimation() {
@@ -37,5 +91,40 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void clickSetting(View view) {
+    }
+
+    private class HomeAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mDatas.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mDatas.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = View.inflate(HomeActivity.this, R.layout.item_home, null);
+
+            HomeBean bean = (HomeBean) getItem(position);
+            ImageView ivIcon = (ImageView) convertView.findViewById(R.id.item_home_iv_icon);
+            TextView tvTitel = (TextView) convertView.findViewById(R.id.item_home_tv_title);
+            TextView tvDesc = (TextView) convertView.findViewById(R.id.item_home_tv_desc);
+
+            // 给控件设置数据
+            ivIcon.setImageResource(bean.getIcon());
+            tvTitel.setText(bean.getTitle());
+            tvDesc.setText(bean.getDesc());
+
+            return convertView;
+        }
     }
 }
---
+++ app/src/main/java/org/me/mobilesecurity/bean/HomeBean.java
@@ -0,0 +1,32 @@
+package org.me.mobilesecurity.bean;
+
+
+public class HomeBean {
+    private int icon;
+    private String title;
+    private String desc;
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
