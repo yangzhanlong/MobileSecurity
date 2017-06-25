bd4220112cca339a2b7d843e3e3393805fa31703
Zhanlong Yang <673029686@qq.com>
2017-06-25 15:06:53 +0800

    Add download apk when upgrade.

--- app/src/main/java/org/me/mobilesecurity/activity/SplashActivity.java
+++ app/src/main/java/org/me/mobilesecurity/activity/SplashActivity.java
@@ -1,14 +1,25 @@
 package org.me.mobilesecurity.activity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,6 +27,9 @@ import org.me.mobilesecurity.R;
 import org.me.mobilesecurity.utils.PackageUtils;
 import org.me.mobilesecurity.utils.StreamUtils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
@@ -24,8 +38,13 @@ import java.net.URL;
 public class SplashActivity extends AppCompatActivity {
 
     private TextView mTvVersion;
+    private String mDownloadUrl;
     private HttpURLConnection conn;
+    private static final int REQUEST_CODE = 1;
     private static final int WHAT_SHOW_UPDATE_DIALOG = 100;
+    private static final int WHAT_SHOW_ERROR = 102;
+    private static final int REQUEST_CODE_INSTALL = 101;
+    private ProgressDialog dialog;
 
     private Handler mHandler = new Handler() {
         @Override
@@ -51,13 +70,13 @@ public class SplashActivity extends AppCompatActivity {
         checkVersionUpdate();
     }
 
-    private void checkVersionUpdate(){
+    private void checkVersionUpdate() {
         // 版本更新检测
         // 去网络中获得最新的版本信息
         new Thread(new CheckVersionTask()).start();
     }
 
-    private void loadToHome(){
+    private void loadToHome() {
         // 延时操作
         mHandler.postDelayed(new Runnable() {
             @Override
@@ -70,14 +89,13 @@ public class SplashActivity extends AppCompatActivity {
                 Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
-        }, 2000);
+        }, 1500);
     }
 
     /**
      * 检测版本更新的任务
      *
      * @author Administrator
-     *
      */
     private class CheckVersionTask implements Runnable {
         @Override
@@ -85,7 +103,7 @@ public class SplashActivity extends AppCompatActivity {
             try {
                 // 去网络中获得最新的版本信息
                 // **** 从网络中需要获得versionCode
-                URL url = new URL("http://192.168.80.53:8080/info.json");
+                URL url = new URL("http://192.168.56.1:8080/info.json");
                 conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("GET");
                 conn.setConnectTimeout(2 * 1000); // 设置网络连接的超时时间
@@ -111,7 +129,9 @@ public class SplashActivity extends AppCompatActivity {
 
                     Float netVersionName = BigDecimal.valueOf(object.getDouble("versionName")).floatValue();
 
-                    // 2. 根据网络版本和本地版本进行比对，判断是否需要更新
+                    mDownloadUrl = object.getString("url");
+
+                    // 根据网络版本和本地版本进行比对，判断是否需要更新
                     if (netVersionCode > localVersionCode) {
                         // 提醒用户更新
                         showUpdateDialog(content, netVersionName);
@@ -122,9 +142,9 @@ public class SplashActivity extends AppCompatActivity {
                 }
 
             } catch (java.io.IOException e) {
-                e.printStackTrace();
+                showError(getString(R.string.error_101));
             } catch (JSONException e) {
-                e.printStackTrace();
+                showError(getString(R.string.error_102));
             } finally {
                 conn.disconnect();
             }
@@ -153,16 +173,155 @@ public class SplashActivity extends AppCompatActivity {
         builder.setPositiveButton(R.string.update_now, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                downloadNewApk();
             }
         });
         builder.setNegativeButton(R.string.next_time_update, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                loadToHome();
             }
         });
 
         builder.show();
+    }
+
+    private void downloadNewApk() {
+        // 弹出进度的dialog
+        dialog = new ProgressDialog(this);
+        dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        dialog.setCancelable(false);
+
+        // 注册权限
+        if (ContextCompat.checkSelfPermission(SplashActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // 没有权限。
+            if (ActivityCompat.shouldShowRequestPermissionRationale(SplashActivity.this,
+                    Manifest.permission.READ_CONTACTS)) {
+                // 用户拒绝过这个权限了，应该提示用户，为什么需要这个权限。
+            } else {
+                // 申请授权。
+                ActivityCompat.requestPermissions(SplashActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE);
+            }
+        } else {
+            // 去网络下载apk
+            dialog.show();
+            new Thread(new DownloadApkTask(dialog)).start();
+        }
+    }
+
+    private class DownloadApkTask implements Runnable {
+        private ProgressDialog mDialog;
+
+        public DownloadApkTask(ProgressDialog dialog) {
+            this.mDialog = dialog;
+        }
+
+        @Override
+        public void run() {
+            FileOutputStream fos = null;
+            InputStream inputStream = null;
+            String url = mDownloadUrl;
+            try {
+                conn = (HttpURLConnection) new URL(url).openConnection();
+                conn.setConnectTimeout(5 * 1000);
+                conn.setReadTimeout(5 * 1000);
+
+                inputStream = conn.getInputStream();
+
+                // 获得要下载文件的大小
+                int contentLength = conn.getContentLength();
+                dialog.setMax(contentLength);
+
+                // 指定输出的apk文件,sdcard下
+                File file = new File(Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_DOWNLOADS).getPath(),
+                        System.currentTimeMillis() + ".apk");
+
+                // 写到文件中
+                fos = new FileOutputStream(file);
+
+                int len = -1;
+                int progress = 0;
+                byte[] buffer = new byte[1024];
+
+                // 反复的读写输入流
+                while ((len = inputStream.read(buffer)) != -1) {
+                    fos.write(buffer, 0, len);
+                    progress += len;
+                    // 设置进度
+                    dialog.setProgress(progress);
+                    Thread.sleep(5);
+                }
+                dialog.dismiss();
+
+                // 提示安装
+                installApk(file);
+
+            } catch (IOException e) {
+                showError(getString(R.string.error_103));
+            } catch (InterruptedException e) {
+                showError(getString(R.string.error_104));
+            } finally {
+                StreamUtils.closeIO(fos);
+                StreamUtils.closeIO(inputStream);
+            }
+        }
+    }
+
+    private void showError(String error) {
+        Message msg = Message.obtain();
+        msg.what = WHAT_SHOW_ERROR;
+        msg.obj = error;
+        mHandler.sendMessage(msg);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    dialog.show();
+                    new Thread(new DownloadApkTask(dialog)).start();
+                } else {
+                    Toast.makeText(this, R.string.deny_perm, Toast.LENGTH_SHORT).show();
+                    loadToHome();
+                }
+                return;
+        }
+    }
+
+    private void installApk(File file) {
+        // 发送隐式意图去安装apk
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+
+        // 7.0使用FileProvider获取 apk uri
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) { // 7.0+以上版本
+            Uri apkUri = FileProvider.getUriForFile(this, "org.me.mobilesecurity.fileprovider", file);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.setDataAndType(apkUri,
+                    "application/vnd.android.package-archive");
+        } else {
+            intent.setDataAndType(Uri.fromFile(file),
+                    "application/vnd.android.package-archive");
+        }
+
+        startActivityForResult(intent, REQUEST_CODE_INSTALL);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_INSTALL) {
+            // 响应的是安装的请求结果
+            if (resultCode == Activity.RESULT_OK) {
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                loadToHome();
+            }
+        }
     }
 }
