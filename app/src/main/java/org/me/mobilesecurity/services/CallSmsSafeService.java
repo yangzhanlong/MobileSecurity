a84bb903b553683856d36eb48f2503b5f05bb679
Zhanlong Yang <673029686@qq.com>
2017-07-29 14:36:30 +0800

    Add PhoneStateListener to end call

--- app/src/main/java/org/me/mobilesecurity/services/CallSmsSafeService.java
+++ app/src/main/java/org/me/mobilesecurity/services/CallSmsSafeService.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import org.me.mobilesecurity.bean.BlackBean;
@@ -16,6 +18,8 @@ public class CallSmsSafeService extends Service {
     private static final String TAG = "CallSmsSafeService";
     private BlackDao mDao;
     private SmsReceiver mSmsReceiver;
+    private TelephonyManager mTm;
+    private CallListener mCallListener;
 
     public CallSmsSafeService() {
     }
@@ -41,13 +45,26 @@ public class CallSmsSafeService extends Service {
         filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
         filter.addAction("android.provider.Telephony.SMS_RECEIVED");
         registerReceiver(mSmsReceiver, filter);
+
+        // 2. 电话拦截
+        // 1) 知道电话什么时候拨入
+        // 2) 通过代码去挂电话
+        mTm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+        mCallListener = new CallListener();
+        // 注册电话的监听
+        mTm.listen(mCallListener, PhoneStateListener.LISTEN_CALL_STATE);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "关闭骚扰拦截的服务");
+        // 注销短信接受者
         unregisterReceiver(mSmsReceiver);
+
+        // 注销电话的监听
+        mTm.listen(mCallListener, PhoneStateListener.LISTEN_NONE);
     }
 
     private class SmsReceiver extends BroadcastReceiver {
@@ -69,4 +86,57 @@ public class CallSmsSafeService extends Service {
             }
         }
     }
+
+
+    private class CallListener extends PhoneStateListener {
+        @Override
+        public void onCallStateChanged(int state, final String incomingNumber) {
+            // * @see TelephonyManager#CALL_STATE_IDLE:闲置状态
+            // * @see TelephonyManager#CALL_STATE_RINGING:响铃状态
+            // * @see TelephonyManager#CALL_STATE_OFFHOOK:摘机状态，接听电话状态
+            // state
+            // incomingNumber：拨入的号码
+
+            if (state == TelephonyManager.CALL_STATE_RINGING) {
+                // 响铃时，正在拨入时
+                int type = mDao.findType(incomingNumber);
+                // 判断是否是被拦截的电话
+                if (type == BlackBean.TYPE_CALL || type == BlackBean.TYPE_ALL) {
+                    // 通过代码去挂断电话
+                    Log.d(TAG, "通过代码去挂断" + incomingNumber + "电话");
+                    //endCall();
+                }
+            }
+        }
+    }
+
+    /**
+     * 挂断电话
+     */
+//    private void endCall() {
+//        // 1) ITelephony 实例
+//        // a. ITelephony.aidl添加到代码中--》ITelephony.java
+//        ITelephony iTelephony = null;
+//        // ITelephony.Stub.asInterface(ServiceManager.getService(Context.TELEPHONY_SERVICE));
+//
+//        // IBinder ibinder = ServiceManager
+//        // .getService(Context.TELEPHONY_SERVICE);
+//        try {
+//            // 获取到ServiceManager 的类名
+//            Class<?> clazz = Class
+//                    .forName("android.os.ServiceManager");
+//            // 暴力反射（第一个参数：表示方法名字，第二个参数：表示方法的类型）
+//            Method method = clazz.getMethod("getService",
+//                    String.class);
+//            // 调用当前的方法（第一个参数：表示谁调用当前方法）
+//            IBinder ibinder = (IBinder) method.invoke(null,
+//                    Context.TELEPHONY_SERVICE);
+//            iTelephony = ITelephony.Stub.asInterface(ibinder);
+//
+//            // 2) ITelephony.endCall();挂断电话
+//            iTelephony.endCall();// 异步操作
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
